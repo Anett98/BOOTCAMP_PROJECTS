46c3999ba5377ead7256f747b1429a54a523c94d
Anett <89252038+Anett98@users.noreply.github.com>
2022-09-01 09:54:37 +0400

    Shop with cart

---
+++ app.js
@@ -0,0 +1,121 @@
+const data = '[{"name":"LACE CORSETRY-INSPIRED DRESS","src":"https://static.zara.net/photos///2022/I/0/1/p/7901/414/800/2/w/731/7901414800_2_1_1.jpg?ts=1660301226935","price":"21,900.00 AMD"},{"name":"PLAYSUIT WITH SWEETHEART NECKLINE","src":"https://static.zara.net/photos///2022/I/0/1/p/7901/405/800/2/w/342/7901405800_2_2_1.jpg?ts=1660301224097","price":"24,900.00 AMD"},{"name":"DRESS WITH SHOULDER PADS AND GATHERING","src":"https://static.zara.net/photos///2022/I/0/1/p/7901/413/800/2/w/342/7901413800_2_1_1.jpg?ts=1660301240038","price":"19,900.00 AMD"},{"name":"SATIN-EFFECT DRESS WITH EMBROIDERY","src":"https://static.zara.net/photos///2022/I/0/1/p/0881/205/800/2/w/731/0881205800_2_3_1.jpg?ts=1660218541045","price":"32,900.00 AMD"},{"name":"SATIN DRESS WITH CUT-OUT DETAIL","src":"https://static.zara.net/photos///2022/I/0/1/p/4432/201/800/2/w/342/4432201800_2_5_1.jpg?ts=1660218542966","price":"29,900.00 AMD"},{"name":"MIDI DRESS","src":"https://static.zara.net/photos///2022/I/0/1/p/3067/206/423/2/w/342/3067206423_2_3_1.jpg?ts=1660218542769","price":"24,900.00 AMD"},{"name":"PRINTED WRAP DRESS","src":"https://static.zara.net/photos///2022/I/0/1/p/7563/248/330/2/w/731/7563248330_1_1_1.jpg?ts=1660036173877","price":"49,900.00 AMD"},{"name":"PRINTED MIDI DRESS","src":"https://static.zara.net/photos///2022/I/0/1/p/4437/258/084/2/w/342/4437258084_1_1_1.jpg?ts=1660214784439","price":"32,900.00 AMD"},{"name":"PRINTED DRESS WITH BELT","src":"https://static.zara.net/photos///2022/I/0/1/p/5216/255/064/2/w/342/5216255064_2_2_1.jpg?ts=1660214771080","price":"32,900.00 AMD"},{"name":"FLOWING MIDI DRESS","src":"https://static.zara.net/photos///2022/I/0/1/p/4437/259/400/2/w/731/4437259400_1_1_1.jpg?ts=1660036171715","price":"32,900.00 AMD"},{"name":"ANIMAL PRINT DRESS","src":"https://static.zara.net/photos///2022/I/0/1/p/0387/176/330/2/w/731/0387176330_1_1_1.jpg?ts=1660291342141","price":"29,900.00 AMD"},{"name":"MIDI DRESS WITH GATHERED DETAIL","src":"https://static.zara.net/photos///2022/I/0/1/p/8342/210/800/12/w/342/8342210800_1_1_1.jpg?ts=1660288882690","price":"32,900.00 AMD"},{"name":"BUTTONED MIDI DRESS","src":"https://static.zara.net/photos///2022/I/0/1/p/7969/240/446/2/w/313/7969240446_1_1_1.jpg?ts=1660303518411","price":"32,900.00 AMD"},{"name":"LONG PRINTED DRESS","src":"https://static.zara.net/photos///2022/I/0/1/p/5216/247/500/3/w/190/5216247500_1_1_1.jpg?ts=1660113757565","price":"32,900.00 AMD"},{"name":"SEQUINNED MIDI DRESS","src":"https://static.zara.net/photos///2022/I/0/1/p/7521/252/330/4/w/190/7521252330_2_1_1.jpg?ts=1659364520271","price":"85,900.00 AMD"},{"name":"LONG PRINTED DRESS","src":"https://static.zara.net/photos///2022/I/0/1/p/5598/045/500/2/w/190/5598045500_1_1_1.jpg?ts=1655449301432","price":"32,900.00 AMD"},{"name":"DENIM PLAYSUIT","src":"https://static.zara.net/photos///2022/I/0/1/p/1889/160/712/2/w/178/1889160712_1_1_1.jpg?ts=1660303497696","price":"24,900.00 AMD"},{"name":"DENIM PLAYSUIT","src":"https://static.zara.net/photos///2022/I/0/1/p/1889/160/427/2/w/178/1889160427_2_3_1.jpg?ts=1660303489411","price":"24,900.00 AMD"},{"name":"LACE-TRIMMED DRESS WITH ELASTIC STRAPS","src":"https://static.zara.net/photos///2022/I/0/1/p/7803/616/800/2/w/190/7803616800_2_1_1.jpg?ts=1660302659500","price":"24,900.00 AMD"},{"name":"TIE-DYE PRINT CAMISOLE DRESS","src":"https://static.zara.net/photos///2022/I/0/1/p/7766/240/330/2/w/190/7766240330_1_1_1.jpg?ts=1660215142110","price":"29,900.00 AMD"},{"name":"PLEATED MIDI DRESS","src":"https://static.zara.net/photos///2022/I/0/1/p/2298/178/712/2/w/190/2298178712_1_1_1.jpg?ts=1660215115054","price":"39,900.00 AMD"},{"name":"SATIN MINI DRESS WITH BELT","src":"https://static.zara.net/photos///2022/I/0/1/p/2298/185/505/2/w/190/2298185505_2_1_1.jpg?ts=1660215115105","price":"29,900.00 AMD"},{"name":"FLORAL CAMISOLE MIDI DRESS","src":"https://static.zara.net/photos///2022/I/0/1/p/1165/169/982/2/w/265/1165169982_1_1_1.jpg?ts=1660215113866","price":"29,900.00 AMD"},{"name":"EMBROIDERED DRESS","src":"https://static.zara.net/photos///2022/I/0/1/p/7901/815/898/2/w/265/7901815898_2_2_1.jpg?ts=1660302169866","price":"24,900.00 AMD"},{"name":"CUT-OUT JACQUARD DRESS","src":"https://static.zara.net/photos///2022/I/0/1/p/5039/709/518/2/w/178/5039709518_2_2_1.jpg?ts=1660302189708","price":"19,900.00 AMD"},{"name":"EMBROIDERED DRESS","src":"https://static.zara.net/photos///2022/I/0/1/p/7901/814/529/2/w/178/7901814529_2_2_1.jpg?ts=1660302168562","price":"24,900.00 AMD"},{"name":"KNIT PLEATED MIDI DRESS","src":"https://static.zara.net/photos///2022/I/0/1/p/9598/106/500/2/w/265/9598106500_2_1_1.jpg?ts=1660308591996","price":"29,900.00 AMD"},{"name":"POPLIN SHIRT DRESS","src":"https://static.zara.net/photos///2022/I/0/1/p/3666/211/800/2/w/265/3666211800_2_1_1.jpg?ts=1660303500456","price":"22,900.00 AMD"}]'
+let mainBody = document.querySelector('.Body')
+let openBag = document.querySelector('.fa-shopping-bag')
+let cart = document.querySelector('.bagPage')
+let closeBag = document.querySelector('.fa-close')
+let getCart = document.querySelector('.addDiv')
+let actionsOnMyCart = document.querySelector('.bagPage')
+let searchName = document.querySelector('.nameClass')
+
+ 
+
+function fetchedData () {
+    return new Promise((res)=>{
+        res(data)
+    })
+}
+
+const pruducts = () => {
+     fetchedData().then((res)=>JSON.parse(res)).then((res) => {
+        res.map(el=>{
+            let ourDiv = document.createElement('div')
+            mainBody.append(ourDiv)
+            ourDiv.className = 'results';
+            ourDiv.innerHTML =`<img class="imgResult" src="${el.src}" alt="Dress">
+            <div class = "nameClass">${el.name}</div> 
+            <div class="priceClass">${el.price}</div> 
+            <div class="divForButtons">
+            <button class="buyButton">ADD</button>
+            <button type="submit" class="likeClass"><i class = "fas fa-heart"></i></button>
+            </div>`
+        })
+    })
+}
+
+
+
+
+pruducts()
+
+ openBag.onclick = () => {
+    cart.classList.add('active')
+ }
+ closeBag.onclick = () => {
+   cart.classList.remove('active')
+ }
+
+
+
+
+mainBody.addEventListener("click", buttonFunc)
+function buttonFunc(event) {
+    const target = event.target;
+    if(target.classList[0] === 'buyButton') {
+       const imported = target.parentElement.parentElement;
+       
+       let name = imported.querySelector('.nameClass')
+       let price = imported.querySelector('.priceClass')
+       let img = imported.querySelector('.imgResult')
+       let bagDiv = document.createElement('div')
+            getCart.prepend(bagDiv)
+            bagDiv.className = 'listedItems';
+            bagDiv.innerHTML =`<img class="bagImgClass" src="${img.src}" alt="Dress">
+            <div class="details">
+            <div class = "bagProductName">${name.outerText}</div> 
+            <div class="bagProductPrice">${price.outerText}</div>
+            <input type="number" min = "1" value="1" class="amount">
+            </div>
+            <div class ="delete"><i class = "fas fa-trash" ></i></div> `
+   } 
+    if(target.classList[0] === 'likeClass') {
+        const likeItem = target
+        likeItem.classList.toggle("likedItem")
+    }
+}
+
+
+
+
+mainBody.addEventListener("click", getTotal)
+function getTotal () {
+     let getClass = document.getElementsByClassName('bagProductPrice')
+     let newClass = [...getClass]
+     let getCount = document.getElementsByClassName('amount')
+     let amount = [...getCount]
+     const COUNT = amount.map(el=>+el.value)
+     let total = document.querySelector('.totalPrice')
+    if(newClass.length !== 0) { let PRICE_ARR = newClass.map(el=>{
+         let item = el.outerText
+         item=item.slice(0,-7)
+         return +item.split(',').join('')
+        })
+
+     const TOTAL_AMOUNT = PRICE_ARR.map((item,index)=>{
+             return item*COUNT[index]
+            }).reduce((acc,item)=>{
+               return acc+item
+            })
+            total.innerText = TOTAL_AMOUNT+' AMD'
+        } else {
+            total.innerText = 0 +' AMD'
+        }
+}
+
+
+
+
+actionsOnMyCart.addEventListener('click', function (event) {
+    let target = event.target
+    if(target.classList[1] === 'fa-trash') {
+        const listToDelete = target.parentElement.parentElement;
+        listToDelete.remove()
+        getTotal()
+     } 
+     if(target.classList[0] === 'purchaseButton') {
+        alert('Thank you for your order!')
+     }
+     if(target.classList[0]==='amount') {
+        getTotal()
+     }
+})
+
